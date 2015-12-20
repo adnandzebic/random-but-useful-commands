b3eed6604c91265f63b25c1083a65ec863f0e3b4
adnandzebic <adnandzebic@gmail.com>
2015-12-20 04:09:34 -0800

    added pavucontrol + ssh -X

--- commands.sh
+++ commands.sh
@@ -1,5 +1,14 @@
 # Sets HDMI output
 xrandr --output HDMI-2 --mode 1920x1080
 
+# Audio control
+pavucontrol
+
 # Useful for GUI dialogs
 zenity
+
+# Log into pi using VNC
+vncviewer
+
+# Run X11 through SSH
+ssh -X user@hostip
